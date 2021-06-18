99bf0001fcbaa5a92ab7b5679969e3933006f28f
koudai0024 <iwai.0731v@gmail.com>
2021-06-18 22:19:02 +0900

    🐛 依存関係のエラー解消

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  webpack5: false,
+  i18n: { locales: ["ja"], defaultLocale: "ja" },
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,

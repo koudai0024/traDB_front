b8c50a225ee5ea5365707372fb6898fae5591b02
koudai0024 <iwai.0731v@gmail.com>
2021-06-19 17:34:58 +0900

    💄 reset.cssの適応

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import "src/styles/reset.css";
+
+import type { AppProps } from "next/app";
+
+const App = (props: AppProps) => {
+  return (
+    <>
+      <props.Component {...props.pageProps} />
+    </>
+  );
+};
+
+export default App;

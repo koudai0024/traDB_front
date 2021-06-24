d499f016a6cf9abf404092aebfee4277e7519075
koudai0024 <iwai.0731v@gmail.com>
2021-06-24 20:33:01 +0900

    🔨 storybookのpath ailasの修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
+const path = require("path");
+
 module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias["src"] = path.resolve(__dirname, "../src/");
+    return config;
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   core: {

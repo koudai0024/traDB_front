5d56f317d80c09882bef8f0cbba68e2a8f8272fb
koudai0024 <iwai.0731v@gmail.com>
2021-06-24 20:33:55 +0900

    💄 Innerコンポーネントの作成と、Innerコンポーネントのstorybookファイル作成

---
+++ src/components/Common/Inner.stories.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-default-export */
+import type { Meta } from "@storybook/react";
+
+import { Inner } from "./Inner";
+
+export default {
+  title: "Components/Inner",
+  component: Inner,
+} as Meta;
+
+export const Primary = () => {
+  return (
+    <Inner>
+      <h1>hello</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum nulla
+        distinctio incidunt voluptatem, beatae illum ratione facilis. Eveniet
+        omnis vel atque quam similique doloribus pariatur nulla laudantium sint
+        soluta! Necessitatibus.
+      </p>
+    </Inner>
+  );
+};
--- src/components/Common/Inner.tsx
+++ src/components/Common/Inner.tsx
@@ -1,3 +1,15 @@
-export const Inner = () => {
-  return <div></div>;
+import type { ReactNode, VFC } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  border-bottom: 2px solid #d1d5db;
+  padding: 0 0 16px;
+`;
+
+export type InnerProps = {
+  children: ReactNode;
+};
+export const Inner: VFC<InnerProps> = (props) => {
+  return <Container>{props.children}</Container>;
 };
